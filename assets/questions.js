5855f4c0234775a38600c54eccfcf89ff145ad30
Andrea Cannon <cannonac1049@gmail.com>
2024-09-11 09:16:05 -0500

    added questions to javascript

--- assets/questions.js
+++ assets/questions.js
@@ -1,20 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function getUserQuestions(){
+  return [
+    {
+      type: "input",
+      message: "Title of  project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Description of the project?",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Installation instructions?",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "How to use app?",
+      name: "usage"
+    },
+    {
+      type: "list",
+      message: "Licenses?",
+      name: "license",
+      choices: licenseOptions
+    },
+    {
+      type: "input",
+      message: "How to contribute?",
+      name: "contribute"
+    },
+    {
+      type: "input",
+      message: "How to test the project?",
+      name: "test"
+    },
+  ]
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+module.exports = {getUserQuestions}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const licenseOptions = [
+  {
+      name: "Apache - [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  },
+  {
+      name: "boost - [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  },
+  {
+      name: "BSD2 - [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+  },
+  {
+      name: "CC0 - [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"
+  },
+  {
+      name: "GNU GPL v3 - [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  },
+  {
+      name: "IBM Public License - [![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  },
+  {
+      name: "The MIT License - [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  },
+  {
+      name: "Mozilla Public License 2.0 - [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  }
 
-`;
-}
 
-module.exports = generateMarkdown;
+]
